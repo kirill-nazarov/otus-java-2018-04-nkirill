3077036382acc844d038ad664d8cd262962cba53
kirill-nazarov <kirill.v.nazarov@gmail.com>
2018-08-21 22:48:10 +0500

    blank project

--- L08.1-json/src/main/java/ru/otus/l8/Main.java
+++ L08.1-json/src/main/java/ru/otus/l8/Main.java
@@ -1,18 +1,18 @@
 package ru.otus.l8;
 
-
 /*
 ДЗ-08: JSON object writer
 Напишите свой json object writer (object to JSON string) аналогичный gson на основе javax.json или simple-json и Reflection.
-Поддержите массивы объектов и примитивных типов, и коллекции из стандартный библиотерки.
-*/
+Поддержите массивы объектов и примитивных типов, и коллекции из стандартной библиотеки.
+ */
 
 public class Main {
 
-    public static void main(String... args) {
+    public static void main(String[] args) {
 
+        //TODO 1 Write JsonWriter
+        //TODO 2 Write JsonWriter Tests
         System.out.println("Test");
-
     }
 
 }
---
+++ L08.1-json/src/test/java/ru/otus/l8/JsonWriterTest.java
@@ -0,0 +1,12 @@
+package ru.otus.l8;
+
+import org.junit.Test;
+
+
+public class JsonWriterTest {
+
+    @Test
+    public void writeObject() {
+        System.out.println("Test");
+    }
+}
