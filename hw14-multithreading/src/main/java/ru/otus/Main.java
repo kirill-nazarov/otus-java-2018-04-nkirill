329251106316c1683d9e5088e3d5321dc2feba4c
kirill-nazarov <kirill.v.nazarov@gmail.com>
2018-09-20 22:15:30 +0500

    sort array

--- hw14-multithreading/src/main/java/ru/otus/Main.java
+++ hw14-multithreading/src/main/java/ru/otus/Main.java
@@ -1,5 +1,7 @@
 package ru.otus;
 
+import java.util.Arrays;
+
 /**
  * Created by knazarov.
  */
@@ -7,6 +9,22 @@ package ru.otus;
 public class Main {
 
     public static void main(String... args) {
-        System.out.println("Test");
+        int[] array = new int[30];
+        for (int i = 0; i < 30; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
+
+        System.out.println("Not sorted array:");
+        for (int i = 0; i < 30; i++) {
+            System.out.print(" " + array[i]);
+        }
+
+        Arrays.sort(array);
+        System.out.println("");
+        System.out.println("Sorted array:");
+        for (int i = 0; i < 30; i++) {
+            System.out.print(" " + array[i]);
+        }
+
     }
 }
